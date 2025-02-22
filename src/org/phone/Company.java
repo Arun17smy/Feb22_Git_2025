dae6753885d23c3c599de734ef62fe98c2ceeae9
Arun17smy <arun17dinesh@gmail.com>
2025-02-22 11:30:28 +0530

    sandy added a new line

--- src/org/phone/Company.java
+++ src/org/phone/Company.java
@@ -3,7 +3,8 @@ package org.phone;
 public class Company {
 
 	public static void main(String[] args) {
-	System.out.println("Im a new line of code");	
+	System.out.println("Im a new line of code");
+	System.out.println("Sandy added a new line of code");
 	
 	}
 }
